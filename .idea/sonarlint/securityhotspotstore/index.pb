a1e2cc17b10099a6f82bb461acfb03ce748e6eb4
Brandon Alexander <bjvonalexander@gmail.com>
2023-06-12 15:02:56 -0500

    Highlighting Existing Cars
    Finished Application

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,5 +1,3 @@
 
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-J
-src/components/CarValue.js,7/9/79b15363197ee6b546ff7fa89c250985ad094931
--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,9 +3,14 @@ import {removeCar} from "../store";
 
 function CarList() {
     const dispatch = useDispatch();
-    const cars = useSelector(({ cars: { data, searchTerm}}) => {
-        return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    const { cars, name } = useSelector(({ form, cars: { data, searchTerm}}) => {
+        const filteredCars = data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase())
         );
+
+        return {
+            cars: filteredCars,
+            name: form.name
+        };
     });
 
     const handleCarDelete = (car) => {
@@ -15,8 +20,12 @@ function CarList() {
     };
 
     const renderedCars = cars.map((car) => {
+        // Decide if this car should be bold
+        // state.form.name
+        const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
         return (
-            <div key={car.id} className="panel">
+            <div key={car.id} className={`panel ${bold && 'bold'}`}>
                 <p>
                     {car.name} - ${car.cost}
                 </p>
